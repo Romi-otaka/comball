5710a7dffc9617641e4a70d929b803b887333089
Romi-otaka <x23028xx@aitech.ac.jp>
2025-06-17 10:58:20 +0900

    00

--- server.js
+++ server.js
@@ -1,23 +1,63 @@
-// const express =require("express");
-// const app = express();
-// const http = require("http");
-// const server=http.createServer(app);
-// const io = require("socket.io")(server);
-// const PORT=3000;
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const PORT = 3001;
 
-// app.get("/",(req,res)=>{
-//     res.sendFile(__dirname+"/index.html");
-// });
-// io.on("connection",(socket)=>{
-//     console.log("ユーザーが接続しました。")
-//     socket.on("chat message",(msg) => {
-//        // console.log("メッセージ:" + msg);
-//        io.emit("chat message",msg);
-//     });
-// });
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
 
-// server.listen(PORT,()=>{
-//     console.log("litening on 3000");
-// });
+let i = 0;
+let r = Math.floor(Math.random() * 4);  // 0〜3 のランダム
+let questioner;
+let usermode = [0, 0, 0, 0];
+let connectedSockets = [];
 
-console.log("litening on 3000");
+
+io.on("connection", (socket) => {
+    console.log("ユーザーが接続しました。");
+
+
+    if (connectedSockets.length >= 4) {
+        socket.emit("login rejected", "これ以上参加できません。定員に達しています。");
+        socket.disconnect(true);  // 強制切断
+        return;  // それ以上の処理をしない
+    }
+
+    connectedSockets.push(socket);  // ソケットを登録
+
+    socket.on("chat message", (msg) => {
+        socket.data.username = msg;
+        io.emit("cmessage", msg);
+    });
+
+    socket.on("login", () => {
+        socket.data.usernumber = i;
+         socket.emit("user number", socket.data.usernumber);
+         i++;
+         console.log(`${socket.data.usernumber}さんが参加しました。`);
+
+        // 他ユーザーに通知
+        socket.broadcast.emit("user joined", `${socket.data.usernumber}さんが参加しました。`);
+
+        // 4人目がログインしたら出題者を決定して送信
+        if (i === 4) {
+            questioner = r;
+            console.log("出題者は: " + questioner);
+            io.emit("questioner decided", questioner);
+        }
+    });
+
+    socket.on("disconnect", () => {
+        console.log("ユーザーが切断しました。");
+        
+    　　　connectedSockets = connectedSockets.filter(s => s !== socket);
+　　});
+});
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+});
+
